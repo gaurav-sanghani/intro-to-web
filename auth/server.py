bcd77b353905c1f118fb23574006e0b97fce4cd7
Gaurav Sanghani <gaurav.v.sanghani@gmail.com>
2014-11-19 01:07:18 -0600

    Correctly unset cookies in auth module.

--- auth/server.py
+++ auth/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for
 app = Flask(__name__)
 
+import sqlalchemy
+engine = sqlalchemy.create_engine('postgres://app:app@localhost:5432/postgres', convert_unicode=True)
+
 @app.route('/')
 def hello():
 	#return 'helloworld'
@@ -19,16 +22,22 @@ def login():
 	else:
 		return render_template(
 			'login.html',
-			username=request.cookies.get('username'),
-			password=request.cookies.get('password')
+			loggedIn=request.cookies.get('username'),
+			username=request.cookies.get('username', 'guest'),
+			password=request.cookies.get('password', 'none'),
 		)
 
 @app.route('/logout', methods=['GET'])
 def logout():
 	resp = redirect(url_for('hello'))
-	resp.set_cookie('username', '')
-	resp.set_cookie('password', '')
-
+	resp.set_cookie(
+		'username', '',
+		expires='Thu, 01 Jan 1970 00:00:00 GMT'
+	)
+	resp.set_cookie(
+		'password', '',
+		expires='Thu, 01 Jan 1970 00:00:00 GMT'
+	)
 	return resp
 
 if __name__ == '__main__':
